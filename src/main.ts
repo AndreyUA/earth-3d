2104117dca6fd392a05274dc0cddc1d6102b77a2
AndreyUA <infernalsteel666@gmail.com>
2024-08-08 11:27:47 +0300

    added Moon and animated it

--- src/main.ts
+++ src/main.ts
@@ -9,14 +9,18 @@ const gui = new GUI({
   width: 400,
 });
 const debugObject: Record<string, string | number | Function> = {
-  rotationSpeed: 24,
+  earthRotationSpeed: 24,
+  moonRotationSpeed: 10,
+  moonPositionSpeed: 0.2,
 };
 
 // ! Textures
 const textureLoader = new THREE.TextureLoader();
 
-const albedoTexture = textureLoader.load("/earth.jpg");
-albedoTexture.colorSpace = THREE.SRGBColorSpace;
+const earthTexture = textureLoader.load("/earth.jpg");
+earthTexture.colorSpace = THREE.SRGBColorSpace;
+const moonTexture = textureLoader.load("/moon.jpg");
+moonTexture.colorSpace = THREE.SRGBColorSpace;
 
 const sizes = {
   width: window.innerWidth,
@@ -36,14 +40,14 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   100
 );
-camera.position.set(0, 4, 4);
+camera.position.set(0, 5, 6);
 scene.add(camera);
 
 // Earth sphere
 const earth = new THREE.Mesh(
   new THREE.SphereGeometry(2, 32, 16),
   new THREE.MeshPhongMaterial({
-    map: albedoTexture,
+    map: earthTexture,
   })
 );
 // Correct rotation order
@@ -52,6 +56,16 @@ earth.rotation.reorder("ZYX");
 earth.rotateZ((22 * Math.PI) / 180);
 scene.add(earth);
 camera.lookAt(earth.position);
+
+// Moon sphere
+const moon = new THREE.Mesh(
+  new THREE.SphereGeometry(0.3, 32, 16),
+  new THREE.MeshPhongMaterial({
+    map: moonTexture,
+  })
+);
+moon.position.set(4, 0, 0);
+scene.add(moon);
 
 // ! Lights
 const ambientLight = new THREE.AmbientLight("#FFFFFF", 0.3);
@@ -88,7 +102,14 @@ const tick = () => {
   const elapsedTime = clock.getElapsedTime();
 
   // Earth rotation animation
-  earth.rotation.y = (elapsedTime * Math.PI) / +debugObject.rotationSpeed;
+  earth.rotation.y = (elapsedTime * Math.PI) / +debugObject.earthRotationSpeed;
+
+  // Moon position animation
+  moon.position.x = 4 * Math.cos(elapsedTime * +debugObject.moonPositionSpeed);
+  moon.position.z = 4 * Math.sin(elapsedTime * +debugObject.moonPositionSpeed);
+
+  // Moon rotation animation
+  moon.rotation.y = (elapsedTime * Math.PI) / +debugObject.moonRotationSpeed;
 
   // Render
   renderer.render(scene, camera);
@@ -112,8 +133,20 @@ gui
   .step(0.01)
   .name("sunLight Light Intensity");
 gui
-  .add(debugObject, "rotationSpeed")
+  .add(debugObject, "earthRotationSpeed")
   .min(0)
   .max(100)
   .step(1)
   .name("Rotation Speed of Earth (less is faster)");
+gui
+  .add(debugObject, "moonRotationSpeed")
+  .min(0)
+  .max(100)
+  .step(1)
+  .name("Rotation Speed of Moon (less is faster)");
+gui
+  .add(debugObject, "moonPositionSpeed")
+  .min(0)
+  .max(100)
+  .step(1)
+  .name("Changing position of Moon (less is slower)");
